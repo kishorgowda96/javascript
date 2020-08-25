d9c5cb8db04c23490ff0675613da262f1c35a76e
kishorgowda96 <70193501+kishorgowda96@users.noreply.github.com>
2020-08-25 13:26:23 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+﻿function validate() {
+    var name = document.getElementById("Textname").value;
+
+    if (name.length == 0) {
+        sp1.innerHTML = "required name";
+
+    }
+    else {
+        document.getElementById("Textname").innerHTML = "";
+    }
+}
